6f5e52014a80fef517cc83274f499827957aa6d2
shiqi <wdkssq@163.com>
2025-09-10 15:26:05 +0800

    1

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -4,15 +4,16 @@
  * Navbar 配置文件，它在 `.vuepress/plume.config.js` 中被导入。
  */
 
-import { defineNavbarConfig } from 'vuepress-theme-plume'
+import { defineNavbarConfig } from "vuepress-theme-plume";
 
 export default defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  { text: "首页", link: "/", icon: "ant-design:home-twotone" },
+  { text: "博客", link: "/blog/", icon: "ant-design:snippets-outlined" },
+  { text: "标签", link: "/blog/tags/", icon: "ant-design:tag-twotone" },
+  { text: "归档", link: "/blog/archives/", icon: "ant-design:folder-twotone" },
   {
-    text: '笔记',
-    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+    text: "笔记",
+    icon: "ant-design:edit-twotone",
+    items: [{ text: "示例", link: "/notes/demo/README.md" }],
   },
-])
+]);
