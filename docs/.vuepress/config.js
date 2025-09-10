6d418c4b1ab2370c9de0753d7764ee8ed7e28039
shiqi <wdkssq@163.com>
2025-09-10 09:49:14 +0800

    2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/vuepress-docx',
   lang: 'zh-CN',
   title: 'vuepress-docx',
   description: 'vuepress-docx-detail',
